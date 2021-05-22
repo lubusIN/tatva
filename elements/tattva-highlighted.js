53619c87ebe9a92e20a2dc93e803f99ce683b702
Ajit Bohra <ajit@lubus.in>
2021-05-22 15:14:51 +0530

    Add highlighted mvp and demo

---
+++ elements/tattva-highlighted.js
@@ -0,0 +1,81 @@
+/**
+ * Custom Element
+ */
+class TattvaHighlighted extends HTMLElement {
+    // Lifecycle event: executed when the component is inserted into the DOM
+    connectedCallback() {
+        const template = this.renderElement(); 
+        this.attachShadow({ mode: 'open' })
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
+    }
+
+    renderElement() {
+        const size = this.getAttribute('size') ?? '.75rem';
+        const color = this.getAttribute('color') ?? '#000';
+
+        const template = document.createElement('template');
+        template.innerHTML =  `
+            ${this.renderStyle(size, color)}
+
+            <div part="content">
+                <slot></slot>
+            </div>
+            <span class="wrapper" part="wrapper">
+                <span class="spread" part="spread"></span>
+                <span class="dot" part="dot"></span>
+            </span>    
+        `;
+
+        return template;
+    }
+
+    renderSvg() {
+        
+    }
+
+    renderStyle(size, color) {
+        return `
+            <style>
+                .wrapper {
+                    display: flex;
+                    width: ${size};
+                    height: ${size};
+                    position: relative;
+                }
+
+                .spread {
+                    width: 100%;
+                    height: 100%;
+                    opacity: .75;
+                    position: absolute;
+                    border-radius: 9999px;
+                    background-color: ${color};
+                    animation: ping 1s cubic-bezier(0,0,.2,1) infinite;
+                }
+
+                .dot {
+                    width: ${size};
+                    height: ${size};
+                    position: relative;
+                    display: inline-flex;
+                    border-radius: 9999px;
+                    background-color: ${color};
+                }
+
+                @keyframes ping {
+                    75%, 100% {
+                    transform: scale(2);
+                    opacity: 0;
+                    }
+                }
+            </style>
+        `;
+    }
+}
+
+window.TattvaHighlighted = TattvaHighlighted;
+
+// Register Element
+if (!window.customElements.get('tattva-highlighted')) {
+    window.customElements.define('tattva-highlighted', TattvaHighlighted);
+}
