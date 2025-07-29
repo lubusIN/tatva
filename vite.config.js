eff75e4cfb848601317d5c7816fc97fa746f9e56
Ajit Bohra <ajit@lubus.in>
2025-07-29 19:42:59 +0530

    chore: cleanup tailwindcss

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,8 @@
 import { defineConfig } from 'vite';
-import tailwindcss from '@tailwindcss/vite';
 import { viteStaticCopy } from 'vite-plugin-static-copy'
 
-export default defineConfig({
-    build: {
-        rollupOptions: {
-            input: 'tailwind.css',
-            output: {
-                assetFileNames: (assetInfo) => {
-                    if (assetInfo.name === 'tailwind.css') return 'style.css';
-                    return assetInfo.name;
-                },
-            },
-        },
-        outDir: 'website/assets',
-    },
-    plugins: [
-        tailwindcss(),
-        viteStaticCopy({
-            targets: [
-                {
-                    src: 'src/**/*',
-                    dest: './',
-                },
-            ],
-        }),
-    ],
-})
+const config = {
+
+};
+
+export default defineConfig(config)
