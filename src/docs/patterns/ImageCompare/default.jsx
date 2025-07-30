f1faacffb966356b3a6ab75f840643595a19d6fe
Ajit Bohra <ajit@lubus.in>
2025-07-30 22:57:32 +0530

    refactor: cleanup image compare default example

--- src/docs/patterns/ImageCompare/default.jsx
+++ src/docs/patterns/ImageCompare/default.jsx
@@ -5,7 +5,6 @@ function Default() {
     <div>
       {/* @code-start */}
       <tatva-image-compare
-        class="tatva-responsive-compare"
         before="https://d-themes.com/wordpress/udesign/elements/wp-content/uploads/sites/3/2021/08/comparison-4.jpg"
         after="https://d-themes.com/wordpress/udesign/elements/wp-content/uploads/sites/3/2021/08/comparison-3.jpg"
       />
